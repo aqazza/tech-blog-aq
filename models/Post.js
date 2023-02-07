cea740c459f297a53917bf389288149fed572dde
Anas Qazza <anasqazza@gmail.com>
2023-02-07 00:28:56 -0800

    commentroutes updated

--- models/Post.js
+++ models/Post.js
@@ -1,20 +1,31 @@
-
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config");
 
 class Post extends Model {}
 
 Post.init(
   {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
   },
   {
     sequelize,
     freezeTableName: true,
-    modelName: 'post'
+    modelName: "post",
   }
 );
 
 module.exports = Post;
-
